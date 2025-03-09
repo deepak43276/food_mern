ff045253fa43cf6cd8f14e3a7d9a6471569c63fb
deepak43276 <deepak91913@gmail.com>
2025-03-09 10:42:58 +0530

    first commit

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="Logo" />
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="Facebook" />
+            <img src={assets.twitter_icon} alt="Twitter" />
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+94 765489545</li>
+            <li>@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright {currentYear} &copy; - All Rights Reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
